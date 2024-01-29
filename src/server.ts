88b29681372be7ba727bce42f5db2d0a204b4b7a
williansouzh <williansouza4041@gmail.com>
2024-01-28 22:20:34 -0300

    feat(server): Connect database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import app from "./app";
 import dotenv from "dotenv";
+import connectDatabase from "./database/databaseConnect";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-function startServer() {
+async function startServer() {
   if (!PORT) {
   }
-  app.listen(PORT, () => {
-    console.log(`Server running at port: ${PORT}`);
-  });
+  try {
+    await connectDatabase();
+    app.listen(PORT, () => {
+      console.log(`Server running at port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error at connect database. Error: ${error}`);
+  }
 }
 
 startServer();
